424e95a5ba12e7eb45a67577f59949b11cd866e6
Daiwei <daiweilu123@gmail.com>
2015-05-16 16:55:29 -0400

    Update bin and helper

--- bin/static-pipeline.js
+++ bin/static-pipeline.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 
 var path = require('path');
+var assign = require('lodash/object/assign');
 var StaticPipeline = require('../dist');
 var configBlock = require(path.resolve('Staticfile'));
 
-var staticPipeline = new StaticPipeline().config(configBlock);
-staticPipeline.opts.logging = true;
-staticPipeline.run();
+var DEFAULT_OPTS = {
+  logging: true
+};
+
+var config = {};
+
+var options;
+
+configBlock(config);
+
+options = assign({}, DEFAULT_OPTS, config.options);
+
+new StaticPipeline(config.tasks, options).start();
