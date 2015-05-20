2a1c5af02d0a3b4e05e27cd40a691c1a467aad05
Daiwei <daiweilu123@gmail.com>
2015-05-19 21:58:23 -0400

    Handle process error

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -17,6 +17,7 @@ var roll = new StaticPipeline({
     ],
     process: function (pipeline) {
       console.log(`==> ${pipeline.src} -> ${pipeline.dest}`);
+      throw new Error('whatever');
       pipeline.watch();
       pipeline.done();
     }
